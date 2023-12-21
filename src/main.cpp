138c668451ecc94d21039083db00b92dcf4bdb4c
AbdelRahman <abdelrahman.rahal.mail@gmail.com>
2023-12-21 22:21:57 +0200

    Added Supplier class

--- src/main.cpp
+++ src/main.cpp
@@ -63,6 +63,51 @@ public:
 		// Modify spare part information
 	}
 };
+
+
+class Supplier {
+private:
+	string m_supplierName;
+	unsigned m_supplierCode;
+	string m_address;
+	string m_telephone;
+	string m_email;
+
+public:
+	// Getters
+	string name() const { return m_supplierName; }
+	unsigned code() const { return m_supplierCode; }
+	string address() const { return m_address; }
+	string telephone() const { return m_telephone; }
+	string email() const { return m_email; }
+
+	// Setters
+	void setName(string &name) { m_supplierName = name; }
+	void setCode(int code) { m_supplierCode = code; }
+	void setAddress(string &address) { m_address = address; }
+	void setTelephone(string &telephone) { m_telephone = telephone; }
+	void setEmail(string &email) { m_email = email; }
+
+	// Member functions
+	void getData() const {
+		cout << endl;
+		cout << "------------------------------" << endl;
+		cout << "Supplier:\t\t" << name() << endl;
+		cout << "Supplier code:\t" << code() << endl;
+		cout << "Address:\t\t" << address() << endl;
+		cout << "Telephone:\t\t" << telephone() << endl;
+		cout << "Email:\t\t" << email() << endl;
+	}
+
+	void putData() {
+//		cout << "Spare part name:\t";
+//		getline(cin, m_partName);
+	}
+
+	void modifyData() {
+		// Modify spare part information
+	}
+};
 int main() {
 	cout << "Hello, world!" << endl;
 	return 0;
