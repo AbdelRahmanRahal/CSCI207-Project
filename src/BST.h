8470c3c53b5b52bf7806f6fb45e2b20c5bbdf587
AbdelRahman <abdelrahman.rahal.mail@gmail.com>
2023-12-29 05:40:04 +0200

    Added a remove node method

--- src/BST.h
+++ src/BST.h
@@ -69,7 +69,103 @@ public:
 	}
 
 
-	// TODO: Implement delete and other necessary methods
+	// Deleting a node
+	bool remove(unsigned code) {
+		BSTNode<Record, Node>* parent = nullptr;
+		BSTNode<Record, Node>* curr = root();
+		bool isLeftChild = false;
+
+		// Search for the node to be deleted
+		while (curr != nullptr) {
+			if (code == curr->data().record().code())
+				break;
+			parent = curr;
+			if (code < curr->data().record().code()) {
+				isLeftChild = true;
+				curr = curr->left();
+			} else {
+				isLeftChild = false;
+				curr = curr->right();
+			}
+		}
+
+		// Return false if the node with the given code is not found
+		if (curr == nullptr)
+			return false;
+
+		// Node with only one child or no child
+		if (curr->left() == nullptr || curr->right() == nullptr) {
+			BSTNode<Record, Node>* temp = curr->left() ? curr->left() : curr->right();
+
+			// No child case
+			if (temp == nullptr) {
+				if (curr == root()) {
+					setRoot(nullptr);
+				} else {
+					if (isLeftChild)
+						parent->setLeft(nullptr);
+					else
+						parent->setRight(nullptr);
+				}
+			} else { // One child case
+				if (curr == root()) {
+					setRoot(temp);
+				} else {
+					if (isLeftChild)
+						parent->setLeft(temp);
+					else
+						parent->setRight(temp);
+				}
+			}
+
+			delete curr;
+		} else { // Node with two children: get the inorder successor (smallest in the right subtree)
+			BSTNode<Record, Node>* temp = minValueNode(curr->right());
+			curr->setData(temp->data());
+			curr->setRight(deleteNodeHelper(curr->right(), temp->data().record().code()));
+		}
+
+		return true;
+	}
+
+
+	BSTNode<Record, Node>* deleteNodeHelper(BSTNode<Record, Node>* root, unsigned key) {
+		if (root == nullptr) return root;
+
+		// If the key to be deleted is smaller than the root's key,
+		// then it lies in left subtree
+		if (key < root->data().record().code())
+			root->setLeft(deleteNodeHelper(root->left(), key));
+
+			// If the key to be deleted is greater than the root's key,
+			// then it lies in right subtree
+		else if (key > root->data().record().code())
+			root->setRight(deleteNodeHelper(root->right(), key));
+
+			// if key is same as root's key, then this is the node to be deleted
+		else {
+			// node with only one child or no child
+			if (root->left() == nullptr) {
+				BSTNode<Record, Node>* temp = root->right();
+				delete root;
+				return temp;
+			} else if (root->right() == nullptr) {
+				BSTNode<Record, Node>* temp = root->left();
+				delete root;
+				return temp;
+			}
+
+			// node with two children: get the inorder successor (smallest in the right subtree)
+			BSTNode<Record, Node>* temp = minValueNode(root->right());
+
+			// copy the inorder successor's content to this node
+			root->setData(temp->data());
+
+			// delete the inorder successor
+			root->setRight(deleteNodeHelper(root->right(), temp->data().record().code()));
+		}
+		return root;
+	}
 
 
 	// Finding the minimum node
